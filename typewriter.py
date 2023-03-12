7bb9274fa966594a9bef40c92f23d1a4a1b89a14
Gunars Frickauss <gunarsf@gmail.com>
2023-03-12 10:47:33 +0200

    Add files via upload

---
+++ typewriter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+MAX_WIDTH=40
+DELIM=' '
+def format_sagacious_words(str):
+    '''
+    ascii chars only,
+    string to array of lines <= MAX_WIDTH
+    ''' 
+    if len(str) == 0:
+        return(None)
+    r=[]
+
+    tray = ''.join([char for char in str if 32 <= ord(char) <= 126])
+    #Find DELIM closest to MAX_WIDTH
+    while len(tray) >= MAX_WIDTH:
+        delim_pos = tray[:MAX_WIDTH].rfind(DELIM)
+        #If no DELIM, use MAX_WIDTH
+        if delim_pos == -1: delim_pos = MAX_WIDTH
+        r.append(tray[:delim_pos+1])
+        tray = tray[delim_pos+1:]
+    #handle last line
+    r.append(tray)
+    return(r)
+def print_sagacious_words(wisdom):
+    if len(wisdom) == 0:
+        return(None)
+    for line in wisdom:
+        print(line)
+def main():
+    #sagacious_words = input("Enter string\n")
+    sagacious_words = '''Yea, you shouldn't have to go to this much trouble to get VS Code to recognize your virtual environment. The folder is right there in the directory you opened VS Code in. The VS Code team should address this if they truly want to support Python. It cant possibly be that hard'''
+    print_sagacious_words(format_sagacious_words(sagacious_words))
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting")
